d780370ad5499d7d9fc272bd353cac7d0b66962e
Marcel Mroz <mmroz.marcel@gmail.com>
2023-08-27 18:08:41 +0200

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import "./styles/main.scss";
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
 
 function App() {
 
   return (
-    <>
+    <div>
 
-    </>
+    </div>
   )
 }
 
