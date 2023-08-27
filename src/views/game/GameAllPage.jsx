cdd93cffc13aa0024d5a6824f278394ad7b78b59
Marcel Mroz <mmroz.marcel@gmail.com>
2023-08-27 17:39:28 +0200

    first commit

---
+++ src/views/game/GameAllPage.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const GameAllPage = () => {
+  return (
+    <GameAllPageWrapper>
+    </GameAllPageWrapper>
+  )
+}
+
+export default GameAllPage;
+
+const GameAllPageWrapper = styled.div`
+  background-color: var(--clr-violet-dark-active);
+
+  .sc-games{
+    min-height: 100vh;
+    padding-top: 65px;
+  }
+`;
